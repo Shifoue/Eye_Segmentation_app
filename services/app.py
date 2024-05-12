601be886ac62e408b2cbc784076dfccb08bf9621
BRAND Romain <brandromain@yahoo.fr>
2024-05-12 17:19:25 +0200

    Fixed local secure connexion

--- services/app.py
+++ services/app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, flash, request, redirect, url_for
 from flask import render_template
-from flask_talisman import Talisman
+# from flask_talisman import Talisman
 from werkzeug.utils import secure_filename
 
 from backend.AI import AI_process
@@ -11,33 +11,33 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 HOST="0.0.0.0"
 
 app = Flask(__name__)
-talisman = Talisman(app)
+# talisman = Talisman(app)
 
-# Content Security Policy (CSP) Header
-csp = {
-    'default-src': [
-        '\'self\'',
-        'https://code.jquery.com',
-        'https://cdn.jsdelivr.net'
-    ]
-}
-# HTTP Strict Transport Security (HSTS) Header
-hsts = {
-    'max-age': 31536000,
-    'includeSubDomains': True
-}
+# # Content Security Policy (CSP) Header
+# csp = {
+#     'default-src': [
+#         '\'self\'',
+#         'https://code.jquery.com',
+#         'https://cdn.jsdelivr.net'
+#     ]
+# }
+# # HTTP Strict Transport Security (HSTS) Header
+# hsts = {
+#     'max-age': 31536000,
+#     'includeSubDomains': True
+# }
 
-# Enforce HTTPS and other headers
-talisman.force_https = True
-talisman.force_file_save = True
-talisman.x_xss_protection = True
-talisman.session_cookie_secure = True
-talisman.session_cookie_samesite = 'Lax'
-talisman.frame_options_allow_from = 'https://www.google.com'
+# # Enforce HTTPS and other headers
+# talisman.force_https = True
+# talisman.force_file_save = True
+# talisman.x_xss_protection = True
+# talisman.session_cookie_secure = True
+# talisman.session_cookie_samesite = 'Lax'
+# talisman.frame_options_allow_from = 'https://www.google.com'
  
-# Add the headers to Talisman
-talisman.content_security_policy = csp
-talisman.strict_transport_security = hsts
+# # Add the headers to Talisman
+# talisman.content_security_policy = csp
+# talisman.strict_transport_security = hsts
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -67,6 +67,6 @@ def upload_file():
     return render_template("upload.html")
  
 if __name__ == '__main__':
-    app.run(HOST, port=5000)
+    app.run(HOST, port=5000,ssl_context="adhoc")
 
     #AI = AI_process()
