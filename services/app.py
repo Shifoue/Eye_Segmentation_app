bf81cb6bf84f8717146f532dc59606a8a06ca3c0
BRAND Romain <brandromain@yahoo.fr>
2024-06-17 14:43:32 +0200

    Upgraded architectures and added auto restart

--- services/app.py
+++ services/app.py
@@ -29,49 +29,51 @@ def apply_mask(image, mask, alpha = 0.5):
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/')
-def index():
-    return redirect(url_for('home')) #render_template('home.html')
-
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-@app.route('/technical_explanation')
-def technical_explanation():
-    return render_template('technical_explanation.html')
- 
-@app.route('/demonstration', methods=['GET', 'POST'])
-def demonstration():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            origin_image = Image.open(file).convert('RGB')
-
-            origin_image, predicted_mask, masked_image = AI.process(origin_image)
-
-            origin_img_base64 = base64_image_encoder(origin_image)
-            predicted_mask_img_base64 = base64_image_encoder(predicted_mask)
-            masked_image_base64 = base64_image_encoder(masked_image)
-
-            return render_template('demonstration.html', origin_image_data=origin_img_base64, mask_data=predicted_mask_img_base64, masked_image_data=masked_image_base64)
-        
-    return render_template("demonstration.html")
  
 if __name__ == '__main__':
     AI = AI_process()
 
     app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return redirect(url_for('home')) #render_template('home.html')
+
+    @app.route('/home')
+    def home():
+        return render_template('home.html')
+
+    @app.route('/technical_explanation')
+    def technical_explanation():
+        return render_template('technical_explanation.html')
+    
+    @app.route('/demonstration', methods=['GET', 'POST'])
+    def demonstration():
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # If the user does not select a file, the browser submits an
+            # empty file without a filename.
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                origin_image = Image.open(file).convert('RGB')
+
+                origin_image, predicted_mask, masked_image = AI.process(origin_image)
+
+                origin_img_base64 = base64_image_encoder(origin_image)
+                predicted_mask_img_base64 = base64_image_encoder(predicted_mask)
+                masked_image_base64 = base64_image_encoder(masked_image)
+
+                return render_template('demonstration.html', origin_image_data=origin_img_base64, mask_data=predicted_mask_img_base64, masked_image_data=masked_image_base64)
+            
+        return render_template("demonstration.html")
+    
+
     app.run(HOST, port=PORT, ssl_context="adhoc")
